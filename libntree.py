acb42efe7918373577d5fe3e228158b085065a59
vijayskk <vijayskk007@gmail.com>
2025-07-03 21:09:51 +0530

    Human readable sizes

--- libntree.py
+++ libntree.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import humanize
 parser = argparse.ArgumentParser()
 parser.add_argument("path", help="Root directory to display")
 parser.add_argument("--depth", type=int, default=-1, help="Max depth to traverse (-1 = unlimited)")
@@ -26,7 +27,10 @@ def print_tree(path,prefix="",depth=0,max_depth=-1):
             print(prefix+connector+f"📁 {entry}/")
             print_tree(full_path,prefix + ("    " if len(os.listdir(path))-1 == i else "|   "),depth+1,max_depth)
         else:
-            size = os.path.getsize(full_path)
-            print(prefix+connector+f"📄 {entry} ( {size//1024} KB)/")
+            try:
+                size = os.path.getsize(full_path)
+            except:
+                size = 0
+            print(prefix+connector+f"📄 {entry} ({humanize.naturalsize(size,binary=True)})/")
         
 
